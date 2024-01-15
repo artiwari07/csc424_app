3c57a542dd21fe1cdb7281f60eeaca762f8ab2cd
artiwari07 <artiwari@calpoly.edu>
2024-01-15 11:02:26 -0800

    Problem with Step 12 code

--- react-frontend/src/Home.js
+++ react-frontend/src/Home.js
@@ -1,48 +1,49 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "./context/AuthProvider";
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const Home = () => {
-  const { value } = useAuth();
-  const navigate = useNavigate(); // useNavigate hook for navigation
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [loginError, setLoginError] = useState("");
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginError, setLoginError] = useState("");
 
-  const handleLogin = () => {
-    if (username === "bj" && password === "pass424") {
-      value.onLogin();
-      navigate("/landing"); // Use navigate for redirection
-    } else {
-      setLoginError("Invalid username or password. Please try again.");
-    }
-  };
+    const handleLogin = async () => {
+        try {
+            await axios.post("http://localhost:8000/account/login", { userid: username, password });
+            console.log("Login successful");
+            navigate("/landing");
+            console.log("After navigation");
+        } catch (error) {
+            setLoginError("Invalid username or password. Please try again.");
+        }
+    };
 
-  return (
-    <>
-      <h2>Home (Public)</h2>
-      <label>
-        User:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <br />
-      <button type="button" onClick={handleLogin}>
-        Sign In
-      </button>
-      {loginError && <p>{loginError}</p>}
-    </>
-  );
+    return (
+        <>
+            <h2>Home (Public)</h2>
+            <label>
+                User:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </label>
+            <br />
+            <button type="button" onClick={handleLogin}>
+                Sign In
+            </button>
+            {loginError && <p>{loginError}</p>}
+        </>
+    );
 };
